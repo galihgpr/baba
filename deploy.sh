1bddc099bb8db8b11224230f177bef23a095d3d5
Galih Prayogi <galihprayogi25@gmail.com>
2020-07-19 00:19:24 +0700

    ksksks

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/galihgpr/project.git master:gh-pages
+git push -f https://github.com/galihgpr/project.git master:hal-baru
 
 cd -
